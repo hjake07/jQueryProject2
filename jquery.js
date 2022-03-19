afa1483e5b5d6a07e5541dc3444f26cf3c592207
hjake07 <jdavies212@gmail.com>
2022-03-19 09:02:29 -0600

    first commit

---
+++ jquery.js
@@ -0,0 +1,79 @@
+const storeItems = [
+    {
+        name: 'TV',
+        price: 800.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Phone',
+        price: 700.00,
+        inStock: false,
+        details: '5G'
+    },
+    {
+        name: 'Game Console',
+        price: 300.00,
+        inStock: true,
+        details: 'The latest and greatest'
+    },
+    {
+        name: 'Laptop',
+        price: 1200.00,
+        inStock: true,
+        details: '16GB RAM 1TB SSD'
+    },
+    {
+        name: 'Smart Watch',
+        price: 200.00,
+        inStock: false,
+        details: 'Counts your steps'
+    },
+    {
+        name: 'Headphones',
+        price: 100.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Keyboard',
+        price: 100.00,
+        inStock: true,
+        details: 'Types for you'
+    },
+    {
+        name: 'HDMI Cord',
+        price: 100.00,
+        inStock: true,
+        details: 'HDMI to USB type C'
+    },
+    {
+        name: 'Monitor',
+        price: 300.00,
+        inStock: true,
+        details: '4K Ultra HD'
+    },
+    {
+        name: 'Speaker',
+        price: 200.00,
+        inStock: true,
+        details: 'Clearest music to be heard'
+    },
+    {
+        name: 'Video Game',
+        price: 60.00,
+        inStock: true,
+        details: 'Enjoy for hours'
+    },
+];
+storeItems.forEach(function(item){
+    if(item.inStock === true){
+        $('#pTags').append(`<div class ="row"><p class="columnOne">${item.name}</p> <p class="columnTwo">$${item.price}</p>  <p class ="columnThree">${item.details}</p></div>`);
+    } else {
+        console.log('some items not in stock')
+    }
+});
+const changer = document.getElementById('button')
+changer.addEventListener('click', function(){
+    $("#contentContainer").toggleClass("darkClass")
+});
